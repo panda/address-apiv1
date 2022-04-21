d05d47fab59c4cab179b2011858b9e98ddd0afcd
Steven Kotwal <steve@pnada.co>
2022-04-20 20:17:04 -0400

    Add in conditional to find field from data/addresses.json from route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-
+const data = require('./data/address.json')
 // Express allows us to send http methods
 // app.get, app.post, app.delete
 // https://expressjs.com/en/4x/api.html
@@ -13,9 +13,17 @@ app.get('/', (req, res) => {
 
 app.get('/api/v1/addresses', (req, res) => {
     console.log("/api/v1/addresses recieved request");
-    res.send([1, 2, 3, 4, 5]);
+    res.send(data);
+});
+
+// ?/sortBy=name
+app.get('/api/v1/addresses/:line1', (req, res) => {
+    const addresses = data.find(c => c.line1 === req.params.line1);
+    if (!addresses) res.status(404).send('The address was not found'); // return 404
+    res.send(addresses);
 });
 
 // Environment variable for port
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('Listening on port:', port));
+console.log(data);
